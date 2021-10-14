a90f5f284c660eb1a41b8e91d2c4803c130e5c45
anki.singh <ankit.singh@gslab.com>
2021-10-14 16:14:13 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hooks-example/precache-manifest.5d16ba7cf700a3e6deec360e559b5ad5.js"
+  "/react-hooks-example/precache-manifest.8d65547c6368f0cf5f2d7e31245c5124.js"
 );
 
 self.addEventListener('message', (event) => {
